4fdfe382a058048f9b116395ac76927e2560d295
reshetnyak <reshetnjak@gmail.com>
2016-02-20 13:28:22 +0100

    Add removing todo functionality

--- todoApp.js
+++ todoApp.js
@@ -2,23 +2,36 @@
 
 
     var Todo = function(text){
-        this.id   = new Date().getTime();
+        this.id   = '_' + new Date().getTime();
         this.text = text;
         this.completed = false;
         this.archived  = false;
     };
 
+    Todo.prototype.createTodoEl = function(){
+
+        var li = doc.createElement('LI');
+        var textSpan = doc.createElement('SPAN');
+        var closeBtn = doc.createElement('SPAN');
+
+        li.setAttribute('id', this.id);
+        li.classList.add('todo');
+
+        textSpan.textContent = this.text;
+        textSpan.classList.add('todo-text');
+
+        closeBtn.textContent = '   X';
+        closeBtn.classList.add('close-btn');
+
+        closeBtn.addEventListener('click', this.removeTodo.bind(this));
+
+        li.appendChild(textSpan);
+        li.appendChild(closeBtn);
+
+        return li;
+    };
+
     Todo.prototype.appendTodo = function(){
-
-        function createTodo(todo){
-
-            var li = doc.createElement('LI');
-
-            li.setAttribute('id', todo.id);
-            li.innerText = todo.text;
-
-            return li;
-        }
 
         function appendToList(todoLi){
             todoApp
@@ -26,21 +39,32 @@
                 .appendChild(todoLi);
         }
 
-        var todo = this;
-        var todoLi = createTodo(todo);
+        var todoLi = this.createTodoEl();
 
         appendToList(todoLi);
     };
 
+    Todo.prototype.removeTodo = function(){
+        var ul = todoApp.todosUl;
+        var todoToDelete = ul.querySelector('#' + this.id);
+
+        ul.removeChild(todoToDelete);
+    }
+
     var todoApp = {
+        todosNum: 0,
         setAppElements: function(){
             this.addTodoBtn = doc.querySelector('#add-todo-btn');
             this.todoInput  = doc.querySelector('#todo-input');
             this.todosUl    = doc.querySelector('#todos');
             this.todoForm   = doc.querySelector('#todo-form');
+            this.dummyTodo  = doc.querySelector('#dummy-todo');
         },
         addHandlers: function(){
+
             this.todoForm.addEventListener('submit', submitListener);
+
+            this.todoInput.addEventListener('keyup', disableIfEmpty);
 
             function submitListener(e){
 
@@ -54,10 +78,26 @@
                 var todo = new Todo(todoText);
 
                 todo.appendTodo();
+                todoApp.todosNum += 1;
+                clearInput();
+
+                todoApp.todosNum > 0 ? hideDummyTodo() : showDummyTodo();
+
+                function clearInput(){
+                    todoApp.todoInput.value = '';
+                }
+
+                function showDummyTodo(){
+                    todoApp.dummyTodo.classList.remove('hidden');
+                }
+
+                function hideDummyTodo(){
+                    todoApp.dummyTodo.classList.add('hidden');
+                }
 
             }
 
-            this.todoInput.onkeyup = function(){
+            function disableIfEmpty(){
 
                 if ( isInputEmpty() ){
                     isBtnEnabled() && disableBtn();
@@ -76,7 +116,7 @@
                 function disableBtn(){
                     todoApp.addTodoBtn.disabled = true;
                 }
-            };
+            }
 
             function isInputEmpty(){
                 return todoApp.todoInput.value.trim() === '';
